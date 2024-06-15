7d68d86db11b6b09dd6734eb659cffb89325aaf7
Peter Eichinger <peter.eichinger@gmail.com>
2024-06-15 13:51:20 +0200

    add prettier

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,28 @@
-import {
-    Elysia
-} from 'elysia'
+import { Elysia } from 'elysia';
+import { staticPlugin } from '@elysiajs/static';
+import dotenv from 'dotenv';
 
-import { staticPlugin } from '@elysiajs/static'
-
-import dotenv from 'dotenv'
 import { throwUp } from './utils';
 
-
-dotenv.config()
+dotenv.config();
 
 const port = process.env.PORT || 3000;
 
-const files_folder = process.env.FILES_FOLDER ?? throwUp("FILES_FOLDER must be defined");
-
+const files_folder =
+  process.env.FILES_FOLDER ?? throwUp('FILES_FOLDER must be defined');
 
 new Elysia()
-    .use(staticPlugin({ enableDecodeURI: true, assets: files_folder, prefix: "/files" }))
-    .get('/feed.rss', 'Hola World')
-    .listen(port, () => {
-        console.log("Using files from", files_folder)
-        console.log("Listening on", port);
-    });
-
-
-
+  .use(
+    staticPlugin({
+      enableDecodeURI: true,
+      assets: files_folder,
+      prefix: '/files',
+      alwaysStatic: false,
+      staticLimit: 0,
+    }),
+  )
+  .get('/feed.rss', 'Hola World')
+  .listen(port, props => {
+    console.log('Using files from', files_folder);
+    console.log('Listening on', props.url.href);
+  });
