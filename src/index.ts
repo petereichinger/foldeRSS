396a723243ebd2e06a91970162cef2cf9de1a855
Peter Eichinger <peter.eichinger@gmail.com>
2024-06-15 13:20:47 +0200

    Fix url decode

--- src/index.ts
+++ src/index.ts
@@ -7,20 +7,21 @@ import { staticPlugin } from '@elysiajs/static'
 import dotenv from 'dotenv'
 import { throwUp } from './utils';
 
-import { stat } from 'fs/promises';
+
 dotenv.config()
 
 const port = process.env.PORT || 3000;
 
 const files_folder = process.env.FILES_FOLDER ?? throwUp("FILES_FOLDER must be defined");
 
+
 new Elysia()
-    .use(staticPlugin({ assets: files_folder, prefix: "/files" }))
+    .use(staticPlugin({ enableDecodeURI: true, assets: files_folder, prefix: "/files" }))
     .get('/feed.rss', 'Hola World')
-    .listen(port, async () => {
-        const val = await stat(files_folder + "Co ver.jpg");
-        console.log("stats", val)
+    .listen(port, () => {
         console.log("Using files from", files_folder)
         console.log("Listening on", port);
     });
+
+
 
