66d1ec127c10b1c7700168b8a27643c691364a0e
Peter Eichinger <peter.eichinger@gmail.com>
2024-06-16 19:14:32 +0200

    Cleanup code

--- src/generateFeed.ts
+++ src/generateFeed.ts
@@ -74,31 +74,41 @@ async function generateBookItems(
   const bookFsPath = `${options.baseFs}/${author}/${bookName}`;
   const files = await readdir(bookFsPath);
 
-  const chapters = files.filter(item => item.match(/^\d\d \-.*\.mp3$/));
+  const chapters = files.filter(chapter => chapter.match(/^\d\d \-.*\.mp3$/));
 
-  const asyncItems = chapters.map(async item => {
-    const filePath = `${bookFsPath}/${item}`;
-    const fileInfo = await stat(filePath);
-
-    const pubDate = fileInfo.ctime.toUTCString();
-    const path = parsePath(item);
-
-    const fileName = path.name;
-    const [_chapter, title] = fileName.split('-', 2);
-
-    const url = encodeURI(
-      `${options.baseUrl}/${options.prefixUrl}/${author}/${bookName}/${item}`,
-    );
-    return Promise.resolve({
-      title: title.trim(),
-      url,
-      length: fileInfo.size,
-      guid: filePath,
-      pubDate,
-    });
+  const asyncItems = chapters.map(async chapter => {
+    return generateBookItem(options, bookFsPath, author, bookName, chapter);
   });
 
   return await Promise.all(asyncItems);
+}
+
+async function generateBookItem(
+  options: GenerateOptions,
+  bookFsPath: string,
+  author: string,
+  bookName: string,
+  chapter: string,
+) {
+  const filePath = `${bookFsPath}/${chapter}`;
+  const fileInfo = await stat(filePath);
+
+  const pubDate = fileInfo.ctime.toUTCString();
+  const path = parsePath(chapter);
+
+  const fileName = path.name;
+  const [_chapter, title] = fileName.split('-', 2);
+
+  const url = encodeURI(
+    `${options.baseUrl}/${options.prefixUrl}/${author}/${bookName}/${chapter}`,
+  );
+  return Promise.resolve({
+    title: title.trim(),
+    url,
+    length: fileInfo.size,
+    guid: filePath,
+    pubDate,
+  });
 }
 
 function serializeChannel({
@@ -108,15 +118,13 @@ function serializeChannel({
   language,
   items,
 }: Channel): string {
-  return `
- <channel>
+  return `<channel>
   <title>${title}</title>
   <link>${base_url}</link>
   <description>${description}</description>
   <language>${language}</language>
   ${items.map(serializeChannelItems).join('\n')}
-</channel>
-`;
+</channel>`;
 }
 
 function serializeChannelItems({
@@ -125,7 +133,7 @@ function serializeChannelItems({
   length,
   pubDate,
 }: ChannelItem): string {
-  return `<item>
+  return `  <item>
     <title>${title}</title>
     <enclosure url="${url}" length="${length}" type="audio/mpeg" />
     <guid>${url}</guid>
